32d13b8427cede5dc061f667f5a83b282993ea9d
Jaime Maretoli <jaimemaretoli@gmail.com>
2022-07-01 11:08:22 -0300

    Adicionado último exemplo aula 30_06

---
+++ Tratamento de Exceções/Exemplo 5 - Throw e Regex/exThrowExcecaoManual/exThrowExcecaoManual/Estudante.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace exThrowExcecaoManual
+{
+	public class Estudante
+	{
+        public string nome { get; set; }
+        public int id { get; set; }
+
+        public Estudante()
+		{
+		}
+
+        public Estudante(string nome, int id)
+        {
+            this.nome = nome;
+            this.id = id;
+        }
+    }
+
+    [Serializable]
+    class NomeEstudanteInvalidoException : Exception
+    {
+        public NomeEstudanteInvalidoException() { }
+
+        public NomeEstudanteInvalidoException(string nome)
+            : base(String.Format($"\nO nome do estudante foi digitado com caracteres inválidos: {nome}.\n"))
+        {
+
+        }
+    }
+}
---
+++ Tratamento de Exceções/Exemplo 5 - Throw e Regex/exThrowExcecaoManual/exThrowExcecaoManual/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace exThrowExcecaoManual
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Estudante objetoEstudante = new Estudante("Joao", 1); // null, para gerar a exception
+
+            try
+            {
+                ValidacaoEstudante(objetoEstudante);
+                MostrarNomeEstudante(objetoEstudante);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static void MostrarNomeEstudante(Estudante objetoEstudante)
+        {
+            if (objetoEstudante == null)
+            {
+                throw new NullReferenceException("\nO objeto Estudante é nulo. Informe os valores.\n");
+            }
+
+            Console.WriteLine(
+                $"\n- O nome do estudante é: {objetoEstudante.nome}.\n" +
+                $"- O código deste estudante é {objetoEstudante.id}.\n");
+        }
+
+        public static void ValidacaoEstudante(Estudante objetoEstudante)
+        {
+            Regex regex = new Regex("^[a-zA-Z]+$");
+
+            if (!regex.IsMatch(objetoEstudante.nome))
+            {
+                throw new NomeEstudanteInvalidoException(objetoEstudante.nome);
+            }
+        }
+    }
+}
